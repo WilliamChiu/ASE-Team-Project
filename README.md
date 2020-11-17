91a0eff461e25a69112ac74471e6682919e0e5ae
William Chiu <williamchiu16@gmail.com>
2020-11-17 01:29:41 -0500

    some todos

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ASE-Team-Project
 Class project for COMSW4156
+
+Start running with 
+```
+docker-compose up
+```
+
+Server runs at http://localhost:5000
+Client runs at http://localhost:3000
+Mongo GUI runs at http://localhost:8081
--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 const GOOGLE_CLIENT_ID = '900015218011-8vr250fck2dkr7flrcuiljcc1dvh9lvr.apps.googleusercontent.com'
 const GOOGLE_CLIENT_SECRET = 'Wqa3ySu87dLvu_nr5V-9D7IV'
+// TODO
+// Require MongoClient and implement database logic
 
 var userProfile
 
@@ -14,6 +16,13 @@ passport.use(new GoogleStrategy({
     callbackURL: "http://localhost:5000/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
+    // TODO
+    // We do not need to worry about storing sessions on database
+    // Express handles sessions for us with cookies or some shit
+    // When people login though we need to create an account for them on
+    // the database if it does not exist yet
+    // This is the callback from Google, so this is where we check if
+    // a user has an account, and if not, we create one from them
     if (profile?._json?.hd !== "columbia.edu" && profile?._json?.hd !== "barnard.edu")
       done(null, false, {message: "Not a Columbia/Barnard email"})
     userProfile=profile
@@ -37,6 +46,31 @@ app.get('/auth/google/callback',
 app.get('/success', (req, res) => res.send(userProfile))
 app.get('/error', (req, res) => res.send("error logging in"))
 
+// TODO
+// Find out how to auth protect our endpoints ("/api/room/join", "/api/room/send") with Passport middleware
+
+// TODO
+// Use bodyparser https://www.npmjs.com/package/body-parser
+// Get the roomId parameter from the POST request
+// Get their account identifier (whatever that may be, an email, accountId field, idk)
+// If valid, update their entry in database to reflect they are in a new room
+app.post("/api/room/join", (req, res) => {
+
+})
+
+// TODO
+// Use bodyparser https://www.npmjs.com/package/body-parser
+// Get the roomId parameter from the POST request
+// Get their account identifier (whatever that may be, an email, accountId field, idk)
+// If valid, do nothing for now, since there is no websocket architecture
+app.post("/api/room/send", (req, res) => {
+
+})
+
+// TODO
+// Figure out how to create a websocket route for clients to connect to and receive messages
+
+
 passport.serializeUser(function(user, cb) {
   cb(null, user)
 })
@@ -44,8 +78,6 @@ passport.serializeUser(function(user, cb) {
 passport.deserializeUser(function(obj, cb) {
   cb(null, obj)
 })
-
-app.set('view engine', 'ejs')
 
 app.use(session({
   resave: false,
